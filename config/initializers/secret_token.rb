9596c74cf85336482112cb2a7fe05faf54272cbe
Joe Connor <joe.connor@livestation.com>
2012-04-24 08:59:06 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evidencr::Application.config.secret_token = '1cf1b3a71cc38676faeb5e160856df9b922a7b14cb821d2efc965985c1838a4caaf6ce45f3d0a0ae50539c4878f798c3904c9de9164cb8135272452d6aeef3a2'
